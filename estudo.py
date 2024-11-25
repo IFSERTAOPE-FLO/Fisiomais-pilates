96045450fd23eff1afea658666ce85228f35dfa5
wicca12 <alicia.silva@aluno.ifsertao-pe.edu.br>
2024-11-25 09:41:34 -0300

    Create estudo.py

---
+++ estudo.py
@@ -0,0 +1,12 @@
+def ler_numero():
+  numero = float(input("Digite um número: "))
+ 
+def multi_10():
+    resultado = ler_numero * 10
+resultado = multi_10(ler_numero)
+
+print(f"O número multiplicado por 10 é igual à{resultado}:")
+
+multi_10(ler_numero)
+
+
