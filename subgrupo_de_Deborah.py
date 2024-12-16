7d24528391d481618f71a7bd35788c2682fe733f
LeticiaTorres1 <leticia.torres@aluno.ifsertao-pe.edu.br>
2024-12-16 10:40:19 -0300

    Update subgrupo_de_Deborah.py

--- subgrupo_de_Deborah.py
+++ subgrupo_de_Deborah.py
@@ -1,1 +1,11 @@
 #Classes : Aluno, Instrutor, Aula
+
+class Aluno:
+  def __init__(self, id_aluno, nome, peso, altura, condicoes, idade, senha):
+    self._id_aluno = id_aluno
+    self.nome = nome
+    self.peso = peso
+    self.altura = altura
+    self.condicoes = condicoes
+    self.idade = idade
+    self._senha = senha
