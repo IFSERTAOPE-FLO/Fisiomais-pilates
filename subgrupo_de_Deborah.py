3363cfd61fd8df14284e7979f4a5845904f4c199
LeticiaTorres1 <leticia.torres@aluno.ifsertao-pe.edu.br>
2024-12-17 08:51:03 -0300

    Update subgrupo_de_Deborah.py

--- subgrupo_de_Deborah.py
+++ subgrupo_de_Deborah.py
@@ -1,15 +1,30 @@
 #Classes : Aluno, Assinaturas, Login
 
-class Assinatura:
-  def __init__(self, id_aluno, nome, peso, altura, condicoes, idade, senha):
-    self._id_aluno = id_aluno
+class Anamnese:
+  def __init__(self, nome, peso, altura, idade, senha, telefone, endereco, estado_civil, profissao, responsavel):
     self.nome = nome
     self.peso = peso
     self.altura = altura
-    self.condicoes = condicoes
     self.idade = idade
     self.senha = senha
+    self.telefone = telefone
+    self.endereco = endereco
+    self.estado_civil = estado_civil
+    self.profissao = profissao
+    self.responsavel = responsavel
+  
+  def questionario():
+    input("Já praticou pilates antes (S/N)? R = ")
+    input("Pratica ou praticou outras atividades fisícas (S/N)? R = ")
+    input(" (S/N)? R = ")
+
+class Assinatura:
+  def __init__():
 
 class Aluno(Assinatura):
-  def __init__(self, id_aluno, nome, peso, altura, condicoes, idade, senha):
-   super().__init__(id_aluno, nome, peso, altura, condicoes, idade, senha)
+  def __init__(self, assinatura, anamnese):
+   self.assinatura = assinatura
+   self.anamnese = anamnese
+
+assinatura = Assinatura() 
+lana = Aluno(anamnese, assinatura)
