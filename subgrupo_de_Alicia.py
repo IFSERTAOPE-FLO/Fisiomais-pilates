49b3cd8d3beaeec34341f094835bad837f8a679b
cleniospider <clenio.lopes@aluno.ifsertao-pe.edu.br>
2024-12-30 10:53:47 -0300

    subgrupo_de_Alicia.py

--- subgrupo_de_Alicia.py
+++ subgrupo_de_Alicia.py
@@ -39,3 +39,43 @@ class Aluno(Anamnese, Assinaturas):
                 if self.condicoes:
                     print(f"Condições médicas: {', '.join(self.condicoes)}")  
                     print(f"Idade: {self.idade}")
+
+
+# Classe: Aula
+from datetime import datetime
+
+class Aula:
+    def __init__(self, data_hora, duração, tipo_de_aula, instrutor, aluno):
+     
+     if not self._validar_data_hora(data_hora):
+        raise ValueError("o parâmetro 'data_hora' deve ser uma data e hora válida.")
+     
+     self.data_hora = data_hora 
+     self.duração = duração
+     self.tipo_de_aula = tipo_de_aula
+     self.instrutor = instrutor
+     self.aluno = aluno
+     self.ocupado = False 
+
+    def _validar_data_hora(self, valor):
+       return isinstance(valor, datetime)
+    
+    def exibir_horarios(self):
+       estado = "Está Ocupado" if self.ocupado else "Está Vago"
+       print(f"aula de {self.tipo_de_aula} em {self.data_hora.strftime('%d/%m/%y %h:%m')} está {estado}.")
+       return estado
+    
+    def mostrar_duração(self):
+       print(f"duração de aula: {self.duração} em minutos.")
+       return self.duração
+    
+    def exibir_instrutor_responsável(self):
+       print(f"Instrutor Responsável: {self.instrutor.nome}")
+       return self.instrutor.nome
+    
+    def reservar_aula(self):
+       if self.ocupado:
+          print("A aula já está ocupada, desculpe!!")
+       else:
+          self.ocupado = True 
+          print(f"Aula de {self.tipo_de_aula} em {self.data_hora.strftime('%d/%m/%y %h:%m')} foi reservada com sucesso!!")
