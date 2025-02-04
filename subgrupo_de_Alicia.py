93c01a072ea34a3ffb6384f6460522c5a1b688d8
ElvisNogueira <elvis.nogueira150@gmail.com>
2025-02-03 21:07:00 -0300

    corrigindo imports

--- subgrupo_de_Alicia.py
+++ subgrupo_de_Alicia.py
@@ -1,5 +1,5 @@
-from Assinaturas import Assinaturas
-from Anamnese import Anamnese
+from subgrupo_de_Deborah import Assinaturas
+from subgrupo_de_Deborah import Anamnese
 
 class Aluno(Anamnese, Assinaturas):  
     def __init__(self, nome, peso, altura, idade, condicoes=None):
